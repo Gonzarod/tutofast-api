e490dd9d8960d06882f3ff6243b7ac382c9afa29
Jesus Duran Huancas <jesusgonzaloduran@gmail.com>
2021-03-30 22:09:52 -0500

    Project Init

---
+++ src/main/java/com/evertix/tutofastapi/TutofastApiApplication.java
@@ -0,0 +1,13 @@
+package com.evertix.tutofastapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutofastApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutofastApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/evertix/tutofastapi/TutofastApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evertix.tutofastapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutofastApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
